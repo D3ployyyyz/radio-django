ab156b9e831d00ba7107868c68ac45a7140339ea
D3ployyyyz <196260996+D3ployyyyz@users.noreply.github.com>
2025-06-01 23:48:36 -0300

    Update plugins.sh

--- plugins.sh
+++ plugins.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-apt-get update && apt-get install -y ffmpeg
+set -e
+
+# Criar pasta bin se não existir
+mkdir -p bin
+
+# Baixar ffmpeg + ffprobe
+curl -L https://github.com/yt-dlp/FFmpeg-Builds/releases/latest/download/ffmpeg-master-latest-linux64-gpl.zip -o ffmpeg.zip
+unzip -o ffmpeg.zip
+mv ffmpeg-master-latest-linux64-gpl/bin/ffmpeg ./bin/ffmpeg
+mv ffmpeg-master-latest-linux64-gpl/bin/ffprobe ./bin/ffprobe
+chmod +x ./bin/ffmpeg ./bin/ffprobe
+
+# (Opcional) limpar arquivos temporários
+rm -rf ffmpeg.zip ffmpeg-master-latest-linux64-gpl
+
+# Inicia seu app
+python app.py
